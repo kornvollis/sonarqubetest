931869833a7ae9a5e25759f7e4f0fecfe2e75772
Viktor Potharn <viktor.potharn@prezi.com>
2018-04-17 12:36:07 +0200

    add failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import expect from 'expect';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('test 2', () => {
+  const div = document.createElement('div');
+  expect(3).toBe(5);
+});
+
